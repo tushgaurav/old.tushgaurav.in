584ca0aed6f5d4678f6b33e4bb730136d1d8fe29
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2022-09-09 18:59:59 +0530

    initial commit

---
+++ QuoteEngine/quoteEngine.js
@@ -0,0 +1,14 @@
+// import quotes from "./data.json" assert { type: "json" };
+const quotes = require("./data.json");
+const QUOTE_COUNT = quotes.quotes.length;
+
+export function getRandomQuote() {
+  var index = Math.floor(Math.random() * QUOTE_COUNT);
+  return quotes.quotes[index].quote;
+}
+
+export function getRandomQuoteWithAuthor() {
+  var index = Math.floor(Math.random() * QUOTE_COUNT);
+  var quote = quotes.quotes[index];
+  return Object.values(quote);
+}
