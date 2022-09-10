9c5ccf2c9e6492999988e4a18495a93faf57fb44
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2022-09-10 20:08:32 +0530

    removed typos

--- QuoteEngine/quoteEngine.js
+++ QuoteEngine/quoteEngine.js
@@ -1,7 +1,7 @@
 import quotes from "./data.json" assert { type: "json" };
 // const quotes = require("./data.json");
 const QUOTE_COUNT = quotes.quotes.length;
-
+ii;
 export function getRandomQuote() {
   var index = Math.floor(Math.random() * QUOTE_COUNT);
   return quotes.quotes[index].quote;
